468a87a29ea5ce84c2781ce6bbea535865637322
Abir Dutta <abirdutta390@gmail.com>
2024-09-30 10:01:32 +0530

    first commit

---
+++ lib/pages/counter.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _count = 0;
+
+  void increment() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Press to Increase Count'),
+            ElevatedButton(onPressed: increment, child: 
+            Text(_count.toString()))
+          ],
+        ),
+      ),
+    );
+  }
+}
